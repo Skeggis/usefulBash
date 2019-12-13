90f8d842ff3552ebe1d827025e5f58ea37af5829
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2019-12-13 13:34:27 +0000

    Added heroku addon on create

--- createHeroku.sh
+++ createHeroku.sh
@@ -1,8 +1,14 @@
 name=$1
+plan=$2
 
 if [ -z "$name" ]; then
     echo "Please give the app a name"
     exit 1
 fi
 
+if [ -z "$plan"]; then
+    plan="hobby-dev"
+fi
+
 heroku create "$name"
+heroku addons:create heroku-postgresql:"$plan"
