915998edb2296560d965912489e82c0cf997185a
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-02-19 14:25:17 +0000

    WEBSITE_URL

--- createHerokuServer.sh
+++ createHerokuServer.sh
@@ -9,6 +9,7 @@ then
     echo 'For the moment the only supported postgres plan is the free one: "hobby-dev"'
 fi
 read -p "Postgres plan:" POSTGRES_PLAN
+read -p "Redis plan:" REDIS_PLAN
 done
 
 echo Now we need some env variables, thank you!
@@ -16,8 +17,8 @@ echo Now we need some env variables, thank you!
 SANDMAN=("EVENTS_DB" "TICKETS_TYPE_DB" "TAGS_DB" "TAGS_CONNECT_DB" "SPEAKERS_DB"
 "SPEAKERS_CONNECT_DB" "ORGANIZATIONS_DB" "CITIES_DB" "COUNTRIES_DB" "CATEGORIES_DB"
 "SEARCH_EVENTS_DB" "EVENTS_INFO_VIEW" "ORDERS_DB" "CHIRO_TIX_SETTINGS_DB" "CLOUD_NAME"
-"CLOUD_KEY" "CLOUD_SECRET" "ORDERS_DB" "USERS_DB" "SALT" "JWT_SECRET" "TEST" "PAYPAL_CLIENT_SECRET" "PAYPAL_CLIENT_ID")
-# SANDMAN=("EVENTS_DB" "TICKETS_TYPE_DB" "TAGS_DB")
+"CLOUD_KEY" "CLOUD_SECRET" "USERS_DB" "SALT" "JWT_SECRET" "TEST" 
+"PAYPAL_CLIENT_SECRET" "PAYPAL_CLIENT_ID" "PRODUCTION" "WEBSITE_URL")
 
 SET_CONFIGS="heroku config:set"
 for s in "${SANDMAN[@]}"; do
@@ -30,4 +31,5 @@ SET_CONFIGS="$SET_CONFIGS --app=$APP_NAME"
 
 heroku create $APP_NAME
 heroku addons:create heroku-postgresql:$POSTGRES_PLAN
+heroku addons:create heroku-redis:$REDIS_PLAN
 $SET_CONFIGS
